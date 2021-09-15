2e78ebc0b1971ab8282cf5f7899ca748c4beaf69
Kemmochi <fnrkp089@gmail.com>
2021-09-15 14:09:56 +0900

    app_index_api
    api_comment

--- BurgerRanking/app.py
+++ BurgerRanking/app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
-db = client.burger_list
+db = client.project
 
 @app.route('/')
 def home():
@@ -15,13 +15,24 @@ def burgers_list():
     burgers = list(db.hamburger.find({},{'_id':False}).sort('like', -1))
     return jsonify({'result':'success', 'all_burgers': burgers})
 
+#리뷰 불러오기
+@app.route('/get_comment', methods=['GET'])
+def comment_list():
+    burgerid = request.args.get('burgerComment')
+    print(burgerid)
+    comment = list(db.review.find({'burger_id': burgerid} , {'_id':False}))
+    return jsonify({'result': 'success', 'commentList': comment})
 
 # 리뷰작성
 @app.route('/comment', methods=['POST'])
 def burgers_review():
     comment_receive = request.form['comment_give']
-
-    db.hamburger.insert_one(comment_receive)
+    burgerId = request.form['burgerId']
+    doc = {
+        'burger_id': burgerId,
+        'comment': comment_receive
+    }
+    db.review.insert_one(doc)
 
     return jsonify({'result':'success'})
 
@@ -35,6 +46,15 @@ def burgers_like():
     db.hamburger.update_one({'name':name_receive}, {'$set':{'like': new_like}})
     return jsonify({'result':'success'})
 
+# 싫어요
+@app.route('/dislike', methods=['POST'])
+def burgers_dislike():
+    name_receive = request.form['name_give']
+    dislike = db.hamburger.find_one({'name': name_receive})
+    new_dislike = dislike['dislike'] + 1
+
+    db.hamburger.update_one({'name': name_receive}, {'$set': {'dislike': new_dislike}})
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
